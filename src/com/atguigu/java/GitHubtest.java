3b308c7934f90e965607519dd737af47ebd451bf
dengpai <dengpai@126.com>
2021-01-25 17:07:52 +0800

    test

---
+++ src/com/atguigu/java/GitHubtest.java
@@ -0,0 +1,12 @@
+package com.atguigu.java;
+
+/**
+ * @author paipai
+ * @create 2021-01-25 16:47
+ */
+public class GitHubtest {
+
+    public static void main(String[] args) {
+        System.out.println("嘿嘿");
+    }
+}
